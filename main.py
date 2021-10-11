98463a0ce90912225fc6940a6cdc467dc1769d0b
Daniel_CM <daniel_200589@hotmail.com>
2021-10-10 21:37:43 -0500

    Segundo commit

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ feet_input = Entry(frame, width=7, textvariable=feet)
 feet_input.grid(column=1, row=0)
 
 meters = StringVar()
-Label(frame, textvariable=meters)
+meters.set("Ingresa valor")
+Label(frame, textvariable=meters).grid(column=1, row=1)
+
+
+window.mainloop()
